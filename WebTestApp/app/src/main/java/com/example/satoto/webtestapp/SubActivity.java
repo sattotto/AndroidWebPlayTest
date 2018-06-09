b65b1415fc0448f8e2ea5a9f860712b39394015d
佐藤秀輔 <sato.s.timecapsule@gmail.com>
2018-06-09 14:57:49 +0900

    動画再生ができるように

--- WebTestApp/app/src/main/java/com/example/satoto/webtestapp/SubActivity.java
+++ WebTestApp/app/src/main/java/com/example/satoto/webtestapp/SubActivity.java
@@ -3,11 +3,15 @@ package com.example.satoto.webtestapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class SubActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +27,11 @@ public class SubActivity extends AppCompatActivity {
         });
 
         WebView myWebView = (WebView) findViewById(R.id.webView);
+
+        WebSettings settings = myWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
         myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("https://www.google.com/");
+        myWebView.loadUrl("http://live2.nicovideo.jp/watch/lv313468350");
     }
 }
