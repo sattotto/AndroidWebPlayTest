5b34e96040d0172130274d236c62ee56e57ee763
佐藤秀輔 <sato.s.timecapsule@gmail.com>
2018-06-09 15:40:02 +0900

    Fragmentの’表示を追加

--- WebTestApp/app/src/main/java/com/example/satoto/webtestapp/MainActivity.java
+++ WebTestApp/app/src/main/java/com/example/satoto/webtestapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.satoto.webtestapp;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), SubActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button sendFragmentButton = findViewById(R.id.send_fragmrnt_button);
+        sendFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("--------  hoge");
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragmentTransaction.addToBackStack(null);
+
+                fragmentTransaction.replace(R.id.container, TestFragment.newInstance("Fragment"));
+
+                fragmentTransaction.commit();
             }
         });
     }
